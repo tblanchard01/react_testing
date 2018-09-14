0fe5aaa8b2a404f6e8fea698bd0e3737384768a1
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-14 14:36:41 +0100

    added bootstrap

--- es6/classes.js
+++ es6/classes.js
@@ -3,10 +3,21 @@ class Animal {
         this.name = name 
         this.colour = colour 
 // console.log("Hi I'm " + this.name + " and I'm " + this.colour)
-console.log( `Hi I'm ${this.name} and I'm ${this.colour}`)
+
     }
 }
 
-let lion = new Animal("Mufasa", "golden")
-console.log(lion)
 
+class Lion extends Animal {
+    constructor(name, colour, role, home){
+        super(name,colour)
+        this.role = role 
+        this.home = home 
+    }
+    roar(){
+        console.log( `Hi I'm ${this.name} and I'm ${this.colour}, my role is ${this.role}, and I live at ${this.home}`)
+    }
+}
+
+let lion = new Lion("Mufasa", "golden", "king","rock" )
+lion.roar()
