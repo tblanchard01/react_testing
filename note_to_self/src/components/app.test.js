2e8638540c46188be1deced6ab488c6c15c3acdd
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-16 19:08:20 +0100

    8 passing tests

--- note_to_self/src/components/app.test.js
+++ note_to_self/src/components/app.test.js
@@ -28,19 +28,32 @@ describe("App", () => {
   });
   it("renders a submit button", () => {
     expect(
-      app.find(".btn").at(0).text()).toEqual("Submit");
+      app
+        .find(".btn")
+        .at(0)
+        .text()
+    ).toEqual("Submit");
   });
 
   describe("when creating a note", () => {
     let testNote = "test note";
     beforeEach(() => {
-      app.find("FormControl").simulate("change",{
-          target: { value: testNote }
-        });
+      app.find("FormControl").simulate("change", {
+        target: { value: testNote }
+      });
     });
     it("updates the text in state", () => {
-      expect(app.state().text).toEqual(testNote)
+      expect(app.state().text).toEqual(testNote);
+    });
 
+    describe("when submiting a new note", () => {
+      it("adds new note to state array", () => {
+        app
+          .find(".btn")
+          .at(0)
+          .simulate("click");
+        expect(app.state().notes[0].text).toEqual(testNote);
+      });
     });
   });
-}); 
+});
--- note_to_self/src/components/note.test.js
+++ note_to_self/src/components/note.test.js
@@ -7,7 +7,6 @@ const props = {note: {text: 'test_note'} }
 describe('Note', ()=>{
     let note = mount(<Note {...props} />)
     it ('renders the note text', () => {
-        console.log(note.debug()) 
         expect(note.find('p').text()).toEqual(props.note.text)
     })
 
